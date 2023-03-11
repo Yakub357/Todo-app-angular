f6e758f92df908b3b8923daee1241675dff71836
Coding Raccon <egamyakub357@gmail.com>
2023-03-11 19:09:24 +0500

    todo list items rendering and deleting completed ones implemented

--- src/app/components/todos.component.ts
+++ src/app/components/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { Todo } from "../models/Todo";
 
 @Component({
     selector: "app-todos", 
@@ -6,6 +7,34 @@ import { Component, OnInit } from "@angular/core";
     styleUrls: ["./todos.component.css"],
 })
 
-export class TodosComponent {
+export class TodosComponent implements OnInit{
 
+    todos: Todo[] = [];
+    inputTodo: string = "";
+
+    ngOnInit():void {
+        this.todos = [
+            {
+                content: "This is task #1 (not completed)", 
+                completed: false,
+            }, 
+            {
+                content: 'This is task #2 (completed)', 
+                completed: false, 
+            }
+        ]
+    }
+
+    onToggle(id: number): void {
+        this.todos.map((v, i) => {
+            if (i === id) {
+                v.completed = !v.completed
+            }
+            return v
+        })
+    }
+
+    onDelete(): void  {
+        this.todos = this.todos.filter((v) => !v.completed)
+    }
 }
